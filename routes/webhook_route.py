acf7a12f7d36238103c813c1564edc7f34bb2330
S.M.Owais Anas <165829540+dev372k@users.noreply.github.com>
2026-06-04 20:48:04 +0300

    feat "seperate webhook for payment and subscription"

--- routes/webhook_route.py
+++ routes/webhook_route.py
@@ -11,40 +11,14 @@ from repositories.payment_repository import upsert_payment
 
 webhook_router = APIRouter()
 
-@webhook_router.post("/webhook")
-async def webhook(req: Request):
+@webhook_router.post("/webhook/payment")
+async def payment_webhook(req: Request):
     body = await req.json()
     event_type = body["type"]
+
     print(f"Received webhook event: {event_type}")
     
-    if event_type in SubscriptionEvent:
-        wh_response = Subscription(
-            business_id=body["business_id"],
-            event_type=event_type,
-            created_at=body["data"]["created_at"],
-            currency=body["data"]["currency"],
-            status=body["data"]["status"],
-            subscription_id=body["data"]["subscription_id"],
-            product_id=body["data"]["product_id"],
-            customer=Customer(
-                customer_id=body["data"]["customer"]["customer_id"],
-                email=body["data"]["customer"]["email"],
-                name=body["data"]["customer"]["name"],
-                phone_number=body["data"]["customer"]["phone_number"]
-            )
-        )
-
-        await upsert_subscription(wh_response)
-
-        logger.info(
-            "Business=%s Event=%s Subscription ID=%s",
-            wh_response.business_id,
-            wh_response.event_type,
-            wh_response.subscription_id
-        )
-
-        return {"data": wh_response}
-    elif event_type in PaymentEvent:
+    if event_type in PaymentEvent:
         wh_response = Payment(
             business_id=body["business_id"],
             payment_id=body["data"]["payment_id"],
@@ -81,6 +55,43 @@ async def webhook(req: Request):
             wh_response.business_id,
             wh_response.event_type,
             wh_response.payment_id
+        )
+
+        return {"data": wh_response}
+    else:
+        return {"data": "Event type not handled"}
+    
+@webhook_router.post("/webhook/subscription")
+async def subscription_webhook(req: Request):
+    body = await req.json()
+    event_type = body["type"]
+    
+    print(f"Received webhook event: {event_type}")
+    
+    if event_type in SubscriptionEvent:
+        wh_response = Subscription(
+            business_id=body["business_id"],
+            event_type=event_type,
+            created_at=body["data"]["created_at"],
+            currency=body["data"]["currency"],
+            status=body["data"]["status"],
+            subscription_id=body["data"]["subscription_id"],
+            product_id=body["data"]["product_id"],
+            customer=Customer(
+                customer_id=body["data"]["customer"]["customer_id"],
+                email=body["data"]["customer"]["email"],
+                name=body["data"]["customer"]["name"],
+                phone_number=body["data"]["customer"]["phone_number"]
+            )
+        )
+
+        await upsert_subscription(wh_response)
+
+        logger.info(
+            "Business=%s Event=%s Subscription ID=%s",
+            wh_response.business_id,
+            wh_response.event_type,
+            wh_response.subscription_id
         )
 
         return {"data": wh_response}
